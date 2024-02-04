850f78d4c916e5147f12159d8691de13799e610d
sema karan <karan18sema@gmail.com>
2024-02-04 21:47:09 +0100

    refactor code

--- ruby/ceaser_chiper/caesar_cipher.rb
+++ ruby/ceaser_chiper/caesar_cipher.rb
@@ -24,3 +24,27 @@ def caesar_cipher(string, key)
   end
   encyrpted_string.join
 end
+
+
+## TODO: refacotr code with each_char(string enumartor) and user % 26., module
+
+# def caesar_cipher(text, number)
+#   alphabet = ('A'..'Z').to_a
+#   phrase_output = ""
+
+#   text.each_char do |char|
+#     char_upcase = char.upcase
+
+#     if alphabet.include?(char_upcase)
+#       original_index = alphabet.index(char_upcase)
+#       shifted_index = (original_index + number) % 26
+#       shifted_char = alphabet[shifted_index]
+
+#       phrase_output << (char == char_upcase ? shifted_char : shifted_char.downcase)
+#     else
+#       phrase_output << char
+#     end
+#   end
+
+#   puts "Frase codificada: #{phrase_output}"
+# end
