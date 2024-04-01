d52f5eb4c0cddbc9fe75d50bd5bafff26c30f953
sema karan <karan18sema@gmail.com>
2024-04-01 22:55:00 +0200

    update method

--- ruby/ceaser_chiper/caesar_cipher.rb
+++ ruby/ceaser_chiper/caesar_cipher.rb
@@ -1,50 +1,46 @@
-def caesar_cipher(string, key)
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'.chars
-  given_string = string.chars
-  encyrpted_string = []
-  given_string.each do |char|
-    # scenario small letter and 1 alphabet is enough
-    if alphabet.include?(char) && (alphabet.index(char) + key) < 26
-      encyrpted_string.push(alphabet[alphabet.index(char) + key])
-    # scenario with capital letters and 1 alphabet is enough
-    elsif alphabet.include?(char.downcase) && (alphabet.index(char.downcase) + key) < 26
-      encyrpted_string.push(alphabet[alphabet.index(char.downcase) + key].upcase)
-    # scenario small letter and 1 alphabet is not enough
-    elsif alphabet.include?(char) && (alphabet.index(char) + key) > 26
-      new_alphabet = alphabet + alphabet
-      encyrpted_string.push(new_alphabet[alphabet.index(char) + key])
-    # scenario with capital letters and 1 alphabet is not enough
-    elsif alphabet.include?(char.upcase) && (alphabet.index(char.upcase) + key) > 26
-      new_alphabet = alphabet + alphabet
-      encyrpted_string.push(new_alphabet[alphabet.index(char.downcase) + key]).upcase
-    else
-      # adding punctuations, spaces, etc.
-      encyrpted_string.push(char)
-    end
-  end
-  encyrpted_string.join
-end
+# Very naive approach
 
-
-## TODO: refacotr code with each_char(string enumartor) and user % 26., module
-
-# def caesar_cipher(text, number)
-#   alphabet = ('A'..'Z').to_a
-#   phrase_output = ""
-
-#   text.each_char do |char|
-#     char_upcase = char.upcase
-
-#     if alphabet.include?(char_upcase)
-#       original_index = alphabet.index(char_upcase)
-#       shifted_index = (original_index + number) % 26
-#       shifted_char = alphabet[shifted_index]
-
-#       phrase_output << (char == char_upcase ? shifted_char : shifted_char.downcase)
+# def caesar_cipher(string, key)
+#   alphabet = 'abcdefghijklmnopqrstuvwxyz'.chars
+#   given_string = string.chars
+#   encyrpted_string = []
+#   given_string.each do |char|
+#     # scenario small letter and 1 alphabet is enough
+#     if alphabet.include?(char) && (alphabet.index(char) + key) < 26
+#       encyrpted_string.push(alphabet[alphabet.index(char) + key])
+#     # scenario with capital letters and 1 alphabet is enough
+#     elsif alphabet.include?(char.downcase) && (alphabet.index(char.downcase) + key) < 26
+#       encyrpted_string.push(alphabet[alphabet.index(char.downcase) + key].upcase)
+#     # scenario small letter and 1 alphabet is not enough
+#     elsif alphabet.include?(char) && (alphabet.index(char) + key) > 26
+#       new_alphabet = alphabet + alphabet
+#       encyrpted_string.push(new_alphabet[alphabet.index(char) + key])
+#     # scenario with capital letters and 1 alphabet is not enough
+#     elsif alphabet.include?(char.upcase) && (alphabet.index(char.upcase) + key) > 26
+#       new_alphabet = alphabet + alphabet
+#       encyrpted_string.push(new_alphabet[alphabet.index(char.downcase) + key]).upcase
 #     else
-#       phrase_output << char
+#       # adding punctuations, spaces, etc.
+#       encyrpted_string.push(char)
 #     end
 #   end
-
-#   puts "Frase codificada: #{phrase_output}"
+#   encyrpted_string.join
 # end
+
+# more efficient way
+
+def caesar_cipher(text, number)
+  alphabet = ('a'..'z').to_a
+  phrase_output = ''
+  text.chars.each do |char|
+    if alphabet.include?(char.downcase)
+      shifted_index = (alphabet.index(char.downcase) + number) % 26
+      shifted_char = alphabet[shifted_index]
+      phrase_output << (char == char.downcase ? shifted_char : shifted_char.upcase)
+    else
+      phrase_output << char
+    end
+  end
+  phrase_output
+
+end
--- ruby/ceaser_chiper/test.rb
+++ ruby/ceaser_chiper/test.rb
@@ -1,4 +1,4 @@
 require_relative "caesar_cipher"
-p caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 5)
 
-p caesar_cipher("this is Password", 7)
+puts caesar_cipher("this is Password", 7)
