f829be52b71f7e09e1d1a0ac0136abfb3aa3639c
sema karan <karan18sema@gmail.com>
2024-04-09 20:33:09 +0200

    solving tic tac toe game

---
+++ ruby/tic_tac_toe/tic_tac_toe.rb
@@ -0,0 +1,17 @@
+class TicTacToe
+  BOARD= Array.new(9)
+  LINES = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
+
+  # initialize the game
+  # selected cell
+  # update cell
+  # change player
+  # check winner
+
+  def initialize
+
+  end
+
+  def play(marker, index)
+  end
+end
